e314e250b9e1a532cc5d382c5bc51df9d52bab9a
MirrorBottle <setiawanbayu66152@gmail.com>
2019-12-15 13:59:46 +0800

    Done with header

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed3 = new Typed('#headline', {
+    strings: ['<span class="font-weight-light d-block">Nature.</span>', '<span class="font-weight-light d-block"><i>Life.</i></span>', '<span class="d-block"><b>Yourself.</b></span>'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    smartBackspace: true, // this is a default
+    loop: true
+});
